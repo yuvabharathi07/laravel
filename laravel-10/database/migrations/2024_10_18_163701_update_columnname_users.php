9ba604c42b6b97b75ba6ffa80560ef79312e2c62
yuvabharath <yuvabharathi001vp@gmail.com>
2024-10-18 22:15:00 +0530

    auto increment migration

---
+++ laravel-10/database/migrations/2024_10_18_163701_update_columnname_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            // DB::statement();
+            $table->integer('user_id')->autoIncrement()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
